f542ab481d935bae7d51a17a0a281a7f4f5f4f83
charleneChen <satikey@gmail.com>
2026-06-19 00:47:18 +1000

    midified mobile home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,23 @@
 import Image from "next/image";
-import HomePageHeroImage from "@/public/home-page-hero-img.png";
+import AboutPageHeroImage from "@/public/about-page-hero-image.png";
 
-export default function Home() {
+export default function Home() { 
   return (
     <main className="md:flex md:flex-row">
       <section className="px-5 py-10">
-        <p className="uppercase hidden">your go-to platform for 3d printng files</p>
-        <h1 className="font-bold text-4xl">Hi there, I'm Charlene Chen (Xiaolian) · 陈晓莲 </h1>
+        <div className="relative">
+          <div className="w-50 h-50 max-w-full bg-[url(/about-page-hero-image.png)] bg-cover bg-center rounded-full mx-auto mask-b-from-50% mask-b-to-100%"></div>
 
-        <button className="uppercase border-2 p-3 mt-10 font-semibold text-xl">browse models</button>
+          <h1 className="absolute bottom-4 left-0 right-0 font-bold text-4xl text-center text-shadow-sm text-shadow-sky-300">
+            Hi, I'm Charlene!
+          </h1>
+        </div>
+
+        <p className="my-10 text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, quasi. Dicta quis quod quos ipsa vitae nostrum sed odio. Quidem nulla deserunt possimus reiciendis error distinctio, pariatur vitae consequatur aperiam.</p>
+
+        <p className="text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit vel autem quod officiis voluptates error, possimus rerum quam nam consectetur eius vitae, tempora architecto ex iure pariatur dolorum, ipsum assumenda?</p>
       </section>
-      <div>
-        <Image 
-          src={HomePageHeroImage}
-          loading="eager"
-          alt="hero image" />
-      </div>
+
     </main>
   )
 }
