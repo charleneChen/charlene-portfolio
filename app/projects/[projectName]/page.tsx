a221ba3a3e33b167a439832ec44344e7e89766cd
charleneChen <satikey@gmail.com>
2026-06-21 18:15:10 +1000

    remove dynamic route /projects/[projectName] as GitHun Pages is static

--- app/projects/[projectName]/page.tsx
+++
@@ -1,9 +0,0 @@
-import type { ProjectPageProps } from "@/app/types"
-import { getProjectBySlug } from "@/app/lib/projects"
-
-export default async function ProjectPage({ params } : ProjectPageProps) {
-  const { projectName } = await params
-  const project = getProjectBySlug(projectName)
-
-  return <div>{project.displayName}</div>
-}
